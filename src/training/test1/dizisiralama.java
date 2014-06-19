8bb85fa83f731cde9c20bac89bf0adae0d43ee74
mstfbiccer <mstfbiccer@gmail.com>
2014-06-19 18:41:18 +0300

    dizisiralama`nin adlandırmaları kurallara uygun olarak düzenlenmeye çalışıldı.

--- src/training/test1/dizisiralama.java
+++ src/training/test1/dizisiralama.java
@@ -5,36 +5,37 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 public class dizisiralama {
+	private static Scanner scanNumber;
 	public static void main(String[] args) throws IOException
-{
-	Scanner scnf=new Scanner(System.in);
+{	
+	scanNumber = new Scanner(System.in);
 	System.out.println("(1)Rastgele Dizi Oluþturmak-(2) Dosyadan Dizi Çekmek");
-	int sec=Integer.parseInt(scnf.nextLine());
-	if (sec==1)
+	int menuSelect=Integer.parseInt(scanNumber.nextLine());
+	if (menuSelect==1)
 	{
 	System.out.print("Diziniz Kaç Elemanlý Olsun?");
-	int eleman;
-	eleman=Integer.parseInt(scnf.nextLine());
-	System.out.println(eleman+" Elemanlý Diziniz Hazýrlanýyor...");
-	int [] dizi=new int[eleman];
+	int arrayElement;
+	arrayElement=Integer.parseInt(scanNumber.nextLine());
+	System.out.println(arrayElement+" Elemanlý Diziniz Hazýrlanýyor...");
+	int [] dizi=new int[arrayElement];
 	for (int i=0;i<dizi.length;i++)
 	{
 		dizi[i]=(int)(Math.random()*100);
 	}
-	System.out.println(eleman+" Elemanlý Diziniz Yazdýrýlýyor...");
+	System.out.println(arrayElement+" Elemanlý Diziniz Yazdýrýlýyor...");
 	for (int j=0;j<dizi.length;j++)
 	{
 		System.out.println((j+1)+" nci elemanýnýz= "+dizi[j]);
 		
 	}		
 	System.out.println("Diziniz Küçükten Büyüðe Sýralanýyor");
-	sort(dizi, eleman);
-	for (int i = 0; i < eleman; i++)
+	arraySort(dizi, arrayElement);
+	for (int i = 0; i < arrayElement; i++)
 	{	
 	System.out.println((i+1)+"nci elemanýnýz= " + dizi[i]);
 	}
 	}
-	else if (sec==2)
+	else if (menuSelect==2)
 	{	
 		int [] dizi=new int[10];
         File dosya = new File("Inputs/sayilar.txt");
@@ -43,21 +44,18 @@ public class dizisiralama {
         String satir = (reader.readLine());
         int i=0;
         while (satir != null) {
-          	dizi[i]=Integer.parseInt(satir); //amacým burada dosyadan çektiðim deðerlerden dizi oluþturmak ama malesef hata veriyor.
+          	dizi[i]=Integer.parseInt(satir);
           	i++;
         satir = reader.readLine();
-       // System.out.println(satir);
-    }    sort(dizi,i);
+    }    arraySort(dizi,i);
     	for(int j=0;j<i;j++)
     		System.out.println(dizi[j]);
     reader.close();
 }
 	else
 		System.out.println("Lütfen 1 ya da 2 Giriniz");
-}
-
-        
-static void sort(int a[], int size) {
+}        
+static void arraySort(int a[], int size) {
 for (int i = 1; i < size; i++) {
 int x = a[i];
 int j;
