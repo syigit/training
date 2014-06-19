215b5557bdd2c39dc58ef40c407169dd424537c0
mstfbiccer <mstfbiccer@gmail.com>
2014-06-19 18:14:16 +0300

    DiziSiralama duzenlendi.Dosyadan elemanları çekip o şekilde siralıyor.

--- src/training/test1/dizisiralama.java
+++ src/training/test1/dizisiralama.java
@@ -1,9 +1,17 @@
 package training.test1;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 public class dizisiralama {
-public static void main(String args[])
+	public static void main(String[] args) throws IOException
 {
 	Scanner scnf=new Scanner(System.in);
+	System.out.println("(1)Rastgele Dizi Oluþturmak-(2) Dosyadan Dizi Çekmek");
+	int sec=Integer.parseInt(scnf.nextLine());
+	if (sec==1)
+	{
 	System.out.print("Diziniz Kaç Elemanlý Olsun?");
 	int eleman;
 	eleman=Integer.parseInt(scnf.nextLine());
@@ -19,15 +27,34 @@ public static void main(String args[])
 		System.out.println((j+1)+" nci elemanýnýz= "+dizi[j]);
 		
 	}		
-	scnf.close();
 	System.out.println("Diziniz Küçükten Büyüðe Sýralanýyor");
 	sort(dizi, eleman);
 	for (int i = 0; i < eleman; i++)
 	{	
 	System.out.println((i+1)+"nci elemanýnýz= " + dizi[i]);
 	}
-	System.exit(0);
+	}
+	else if (sec==2)
+	{	
+		int [] dizi=new int[10];
+        File dosya = new File("Inputs/sayilar.txt");
+        BufferedReader reader = null;
+        reader = new BufferedReader(new FileReader(dosya));
+        String satir = (reader.readLine());
+        int i=0;
+        while (satir != null) {
+          	dizi[i]=Integer.parseInt(satir); //amacým burada dosyadan çektiðim deðerlerden dizi oluþturmak ama malesef hata veriyor.
+          	i++;
+        satir = reader.readLine();
+       // System.out.println(satir);
+    }    sort(dizi,i);
+    	for(int j=0;j<i;j++)
+    		System.out.println(dizi[j]);
+    reader.close();
 }
+}
+
+        
 static void sort(int a[], int size) {
 for (int i = 1; i < size; i++) {
 int x = a[i];
