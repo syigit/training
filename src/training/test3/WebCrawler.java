f26546b732d018eb7fb9cbbfab1af47a917df5d5
mstfbiccer <mstfbiccer@gmail.com>
2014-07-08 18:15:50 +0300

    duzenlemeler yapildi

--- src/training/test3/WebCrawler.java
+++ src/training/test3/WebCrawler.java
@@ -7,17 +7,29 @@ import java.io.PrintWriter;
 import org.jsoup.*;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
-
-public class webcrawler {
+/**
+ * @author MustafaBICER
+ * @since 08.07.2014
+ */
+public class WebCrawler {
+	/**
+	 * 
+	 * @param args metodumuz string degerli argumanlari icerir
+	 * @throws IOException hata ayýklar
+	 */
 	public static void main(String args[]) throws IOException
 	{
+		/**
+		 * @param doc varsayýlan site dokumanýmýzdýr.
+		 * @oaram links sitedeki doc.select ile seçtiðimiz satýrlarý tutar.
+		 */
 		 Document doc = Jsoup.connect("http://safkoy.com").get();
 		 Elements links = doc.select("a[href]");
 		 System.out.println(links);   
 		 FileWriter outFile = new FileWriter(new File("Inputs/linkler.txt"));
-         PrintWriter out = new PrintWriter(outFile);
-         out.println(links);
-         out.close();
+         PrintWriter fileOut = new PrintWriter(outFile);
+         fileOut.println(links);
+         fileOut.close();
 	}
 	
 }
