b0cdce6f663bbdbed11b119ceb14fb3331ac9e21
mstfbiccer <mstfbiccer@gmail.com>
2014-07-10 14:11:19 +0300

    Class ismi düzenlendi

---
+++ src/training/test3/WebLinkStroge.java
@@ -0,0 +1,41 @@
+package training.test3;
+import java.io.IOException;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import org.jsoup.*;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+/**
+ * @author MustafaBICER
+ * @since 08.07.2014
+ */
+public class WebLinkStroge{
+	/**
+	 * 
+	 * @param args metodumuz string degerli argumanlari icerir
+	 * @throws IOException hata ayiklar
+	 */
+	public static void main(String [] args) throws IOException
+	{ 
+		/**
+		 * @param doc varsayilan site dokumanimizdir.
+		 * @param links sitedeki doc.select ile sectigimiz satirlari tutar.
+		 */
+		 Document doc = Jsoup.connect("http://safkoy.com").get();
+		 Elements links = doc.select("a");
+		 FileWriter outFile = new FileWriter(new File("Inputs/linkler.txt"));
+         PrintWriter fileOut = new PrintWriter(outFile);
+		 for (Element link:links)
+		 {
+			 System.out.println(link.attr("href")); 
+			 fileOut.println(link.attr("href"));	 
+		 }
+        
+         fileOut.close();
+	}
+	
+        
+	}
+
