bd12ae6129ca41e7b4be0e3b60ee7288e855c5e5
Serdar Yiğit <syigitisk@gmail.com>
2014-07-12 18:32:53 +0300

    changed web site and tested

--- src/training/test3/HtmlUnitSimple.java
+++ src/training/test3/HtmlUnitSimple.java
@@ -1,6 +1,8 @@
 package training.test3;
 
 import java.io.IOException;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
@@ -9,7 +11,15 @@ public class HtmlUnitSimple {
 public static void main(String args []) throws FailingHttpStatusCodeException, IOException, IOException
 {
 WebClient browser=new WebClient(); //Creating virtual browser
-HtmlPage page=browser.getPage("http://safkoy.com"); // open web page at browser
+HtmlPage page=browser.getPage("http://www.simpleweb.org/"); // open web page at browser
 System.out.println(page.asText());// print page
+
+
+//final WebClient webClient = new WebClient(BrowserVersion.FIREFOX_17);
+//final HtmlPage page = webClient.getPage("http://htmlunit.sourceforge.net");
+//Assert.assertEquals("HtmlUnit - Welcome to HtmlUnit", page.getTitleText());
+
+browser.closeAllWindows();
+
 }
 }
