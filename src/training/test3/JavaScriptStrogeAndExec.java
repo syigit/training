826dc31df7bb76a2a2a2d2583110eae6c08f6603
mstfbiccer <MyComputer@bicer>
2014-07-23 15:35:29 +0300

    HtmlUnit & Jsoup co-work training

---
+++ src/training/test3/JavaScriptStrogeAndExec.java
@@ -0,0 +1,76 @@
+package training.test3;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+//import java.util.Scanner;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+public class JavaScriptStrogeAndExec {	
+//	private static Scanner scanNumber;
+	public static void main(String [] args) throws IOException
+	{
+		Stroge("http://www.safkoy.com/");
+	}
+	public static void Stroge(String website) throws IOException
+	{	
+		  String adress=website;
+		 Document doc;
+			doc = Jsoup.connect(adress).get();
+			 Elements links = doc.select("script");
+			 FileWriter outFile = new FileWriter(new File("Inputs/scriptler.txt"));
+	         PrintWriter fileOut = new PrintWriter(outFile);
+			 for (Element link:links)
+			 {
+				 if (link.attr("src")!="")
+				 {
+			     System.out.println(website+link.attr("src"));
+				 fileOut.println(website+link.attr("src"));
+				 } 
+			 }
+			  fileOut.close();
+		ExecuteJavaScript(adress);		
+	}
+
+public static void ExecuteJavaScript(String website) throws IOException
+{
+//	scanNumber = new Scanner(System.in);
+	String [] dizi=new String[1000];
+    File dosya = new File("Inputs/scriptler.txt");
+    BufferedReader reader = null;
+    reader = new BufferedReader(new FileReader(dosya));
+    String satir = (reader.readLine());
+    int i=0;
+    WebClient browser=new WebClient();
+    browser.getOptions().setThrowExceptionOnScriptError(false);
+    HtmlPage page=browser.getPage(website); 
+    browser.setAjaxController(new NicelyResynchronizingAjaxController());
+    while (satir != null) {
+          	dizi[i]=satir;
+          	i++;
+        satir = reader.readLine();
+    }
+       for(int j=0;j<i;j++)
+       {
+       //	System.out.println(j+"nci script "+dizi[j]);
+       	page.executeJavaScript(dizi[j]);
+       }
+     
+    /*   System.out.println("Hangi Script Çalýþtýrýlsýn?");
+       int a=Integer.parseInt(scanNumber.nextLine());
+       System.out.println(page.executeJavaScript(dizi[a]));
+       */
+    reader.close();	
+}
+
+}
