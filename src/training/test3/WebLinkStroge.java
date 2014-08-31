399ec73736aa2308d86635b4af067bcdcb3f7d1a
MSTFBICCER <mstfbiccer@gmail.com>
2014-08-31 23:58:33 +0300

    en yeni hal

--- src/training/test3/WebLinkStroge.java
+++ src/training/test3/WebLinkStroge.java
@@ -8,6 +8,7 @@ import java.io.PrintWriter;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Scanner;
 
 import org.jsoup.*;
 import org.jsoup.nodes.Document;
@@ -24,34 +25,41 @@ public class WebLinkStroge{
 	 * @throws IOException hata ayiklar
 	 * @throws URISyntaxException 
 	 */
+	public static Scanner scanPage;
 	public static void main(String [] args) throws IOException, URISyntaxException
 	{ 
-		
+		System.out.println("Taranacak Sayfa Adresini 'xxxx://abc.xyz/' Seklinde Giriniz");
+	    scanPage=new Scanner(System.in);
+		String page=scanPage.nextLine();
+		System.out.println("Taranacak Sayfa: "+page);		
 		/**
 		 * @param doc varsayilan site dokumanimizdir.
 		 * @param links sitedeki doc.select ile sectigimiz satirlari tutar.
 		 */
-		 Document doc = Jsoup.connect("http://demo.segmentify.com").post();
+		
+		 Document doc = Jsoup.connect(page).post();
 		 Elements links = doc.select("a");
-		 FileWriter outFile = new FileWriter(new File("Inputs/linkler.txt"));
+		 FileWriter outFile = new FileWriter(new File("fetch_status.txt"));
          PrintWriter fileOut = new PrintWriter(outFile);
-      Desktop d=Desktop.getDesktop();
+         //Desktop d=Desktop.getDesktop();
          String a;
          Document doc2;
 		 for (Element link:links)
 		 {
 			// System.out.println("http://demo.segmentify.com/"+link.attr("href").replaceAll("http://demo.segmentify.com/", "").replaceAll("//", "")); 
-			 doc2=Jsoup.connect("http://demo.segmentify.com/"+link.attr("href").replaceAll("http://demo.segmentify.com/", "").replaceAll("//", "")).post();
+		 doc2=Jsoup.connect(page+link.attr("href").replaceAll(page, "").replaceAll("//", "")).post();
+			
 			 Elements links2 = doc2.select("a");
 			 for (Element link2:links2)
 			 {
-				// 
-			a="http://demo.segmentify.com/"+link2.attr("href").replaceAll("http://demo.segmentify.com/", "").replaceAll("//", "");
-				 if ( a.contains(".html") && a.length()>60 && a.contains("?")==false && a.contains("http://demo.segmentify.com/home-decor/decorative-accents/"))
+				
+			a=page+link2.attr("href").replaceAll(page, "").replaceAll("//", "");
+				 if ( a.contains(".html"))
 				 { 
+					 fileOut.println(a);
 					 System.out.println(a);
-					 d.browse(new URI(a));
 					 
+					// d.browse(new URI(a));	 
 				 }
 			 }
 			 
