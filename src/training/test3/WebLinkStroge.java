ab24e993fefda724a1412dcc4f8732164fde057b
Mustafa BİÇER <mstfbiccer@gmail.com>
2014-09-03 11:52:47 +0300

    3 saniyede bir erişim

--- src/training/test3/WebLinkStroge.java
+++ src/training/test3/WebLinkStroge.java
@@ -25,11 +25,11 @@ public class WebLinkStroge{
 	 * @throws IOException hata ayiklar
 	 * @throws URISyntaxException 
 	 */
-	public static Selenium selenium,selenium2;
-	
+	public static Selenium selenium,selenium2,selenium3,selenium4;
 	public static Scanner scanPage;
-	public static void main(String [] args) throws IOException, URISyntaxException
-	{ 
+	public static void main(String [] args) throws IOException, URISyntaxException, InterruptedException
+	{ int i =0;
+	
 		System.out.println("Taranacak Sayfa Adresini 'xxxx://abc.xyz/' Seklinde Giriniz");
 	    scanPage=new Scanner(System.in);
 		String page=scanPage.nextLine();
@@ -45,16 +45,17 @@ public class WebLinkStroge{
          PrintWriter fileOut = new PrintWriter(outFile);
      	
           WebDriver driver =new FirefoxDriver();
-        WebDriver driver2 =new FirefoxDriver();
+        WebDriver driver2=new FirefoxDriver();
 		String baseUrl = page;
 		selenium = new WebDriverBackedSelenium(driver, baseUrl);
 		selenium2=new WebDriverBackedSelenium(driver2, baseUrl);
-
          String a;
          Document doc2;
          Elements links2;
 		 for (Element link:links)
 		 {
+			
+			// Thread.sleep(3000);
 			 try
 			 {
 		 doc2=Jsoup.connect(page+link.attr("href").replaceAll(page, "").replaceAll("//", "")).post();
@@ -64,18 +65,19 @@ public class WebLinkStroge{
 			 {continue;}
 			 for (Element link2:links2)
 			 {
+				 i++;
+				 
+				 // Thread.sleep(3000);
 				try
 				{
-					 a=page+link2.attr("href").replaceAll(page, "").replaceAll("//", "");
-					 
+					 a=page+link2.attr("href").replaceAll(page, "");
 					 fileOut.println(a);
 					 System.out.println(a);
-					 selenium.open(a);
-				     selenium2.open("http://qa.n11.com/fortest-P16612836");
-				
+					 if (i%2==0){selenium.open(a);}
+				     selenium2.open(a);
 				}
-				catch(Exception e)
-				{continue;}
+				catch(Exception e){continue;}
 			 } }
          fileOut.close();
-	} }
+	}
+}
